2f689c912b45cadd98c6cee92f5efe011f4b4a52
LeGrandAbi <arsene.brosse9502@gmail.com>
2026-01-13 21:48:06 +0100

    Added difficulty weight to levels, new levels, and did a bit of refactor of update() (of Game())

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
-import pygame as pg 
+import pygame as pg
+from settings import *
 
 
 class Data:
@@ -15,13 +16,89 @@ class Data:
 		self.texture_robot_screen_signal = pg.image.load("./data/textures/robot_screen_signal.png").convert()
 		self.texture_robot_screen_next = pg.image.load("./data/textures/robot_screen_next.png").convert()
 
-		self.easy_levels = [
-				[(0,0), (5,5), (0,5), (5,0)],
-				[(2,2), (2,3), (3,2), (3,3)],
-			]
+		self.easy_levels = []
 		self.normal_levels = []
 		self.hard_levels = []
 
+		self.generate_levels()
+
+	def generate_levels(self):
+	# HARD
+		# left block
+		level = []
+		for y in range(TILEMAP_HEIGHT):
+			for x in range(int(TILEMAP_WIDTH/2)):
+				level.append((x,y))
+		self.hard_levels.append(level)
+
+		# right block
+		level = []
+		for y in range(TILEMAP_HEIGHT):
+			for x in range(int(TILEMAP_WIDTH/2), TILEMAP_WIDTH):
+				level.append((x,y))
+		self.hard_levels.append(level)
+		
+		# top block
+		level = []
+		for y in range(int(TILEMAP_HEIGHT/2)):
+			for x in range(TILEMAP_WIDTH):
+				level.append((x,y))
+		self.hard_levels.append(level)
+		
+		# top block
+		level = []
+		for y in range(int(TILEMAP_HEIGHT/2), TILEMAP_HEIGHT):
+			for x in range(TILEMAP_WIDTH):
+				level.append((x,y))
+		self.hard_levels.append(level)
+		
+	# NORMAL
+		# diagonal
+		level = []
+		for i in range(TILEMAP_HEIGHT):
+			level.append((i,i))
+		self.hard_levels.append(level)
+		
+		# diagonal trans
+		level = []
+		for i in range(TILEMAP_HEIGHT):
+			level.append((i,TILEMAP_HEIGHT-i-1))
+		self.hard_levels.append(level)
+
+		# left block
+		level = []
+		for y in range(TILEMAP_HEIGHT):
+			for x in range((int(TILEMAP_WIDTH/2))-1):
+				level.append((x,y))
+		self.normal_levels.append(level)
+
+		# right block
+		level = []
+		for y in range(TILEMAP_HEIGHT):
+			for x in range(1+(int(TILEMAP_WIDTH/2)), TILEMAP_WIDTH):
+				level.append((x,y))
+		self.normal_levels.append(level)
+		
+		# top block
+		level = []
+		for y in range((int(TILEMAP_HEIGHT/2))-1):
+			for x in range(TILEMAP_WIDTH):
+				level.append((x,y))
+		self.normal_levels.append(level)
+		
+		# top block
+		level = []
+		for y in range(1+(int(TILEMAP_HEIGHT/2)), TILEMAP_HEIGHT):
+			for x in range(TILEMAP_WIDTH):
+				level.append((x,y))
+		self.normal_levels.append(level)
+
+		# center block
+		self.normal_levels.append([(2,2), (2,3), (3,2), (3,3)])
+
+	# EASY
+		#corners
+		self.normal_levels.append([(0,0), (5,5), (0,5), (5,0)])
 
 if __name__ == "__main__":
     import main 
--- game.py
+++ game.py
@@ -22,6 +22,60 @@ class Game(Screen):
 		self.level_timer = 0
 		self.level_period = LEVEL_PERIOD_MAX * 60
 
+	def update(self):
+		self.update_robots()
+		self.update_level()
+
+	def update_robots(self):
+		if self.inputs.keys["primary"].keydown:
+			self.switch_robot_control()
+		for robot in self.robots:
+			robot.update(self.inputs, self.robots)
+			if self.tilemap.collide_with_danger(robot):
+				if robot.controlled:
+					self.switch_robot_control()
+				elif robot.is_next: 
+					self.next_robot.is_next = False
+					self.next_robot = self.robots[randint(0, len(self.robots)-1)]
+					while (self.next_robot == self.controlled_robot) and (len(self.robots) > 1):
+						self.next_robot = self.robots[randint(0, len(self.robots)-1)]
+					self.next_robot.is_next = True
+				self.robots.remove(robot)
+
+	def update_level(self):
+		if self.level_timer == self.level_period/4:
+			self.tilemap.set_warnings(self.current_level)
+		elif self.level_timer == self.level_period*3/4:
+			self.tilemap.set_dangers(self.current_level)
+		elif self.level_timer == self.level_period:
+			self.tilemap.clear_level()
+		elif self.level_timer > self.level_period:
+			self.level_timer = 0
+			self.level_period = max(LEVEL_PERIOD_MIN*60, self.level_period - (LEVEL_PERIOD_MALUS*60))
+			self.change_level()
+		self.level_timer += 1
+
+	def change_level(self):
+		r = randint(1,LEVEL_WEIGHT_EASY + LEVEL_WEIGHT_NORMAL + LEVEL_WEIGHT_HARD)
+		if r <= LEVEL_WEIGHT_EASY :
+			new_lvl = self.data.easy_levels[randint(0, len(self.data.easy_levels)-1)]
+		elif r <= LEVEL_WEIGHT_EASY + LEVEL_WEIGHT_NORMAL :
+			new_lvl = self.data.normal_levels[randint(0, len(self.data.normal_levels)-1)]
+		else:
+			new_lvl = self.data.hard_levels[randint(0, len(self.data.hard_levels)-1)]
+		self.current_level = new_lvl
+
+	def switch_robot_control(self):
+		if len(self.robots) > 1:
+			self.controlled_robot.controlled = False
+			self.controlled_robot = self.next_robot
+			self.controlled_robot.controlled = True
+			self.controlled_robot.is_next = False
+			self.next_robot = self.robots[randint(0, len(self.robots)-1)]
+			while (self.next_robot == self.controlled_robot):
+				self.next_robot = self.robots[randint(0, len(self.robots)-1)]
+			self.next_robot.is_next = True
+
 	def draw_display(self):
 		for x in range(self.tilemap.width):
 			for y in range(self.tilemap.height):
@@ -32,43 +86,8 @@ class Game(Screen):
 			texture = robot.get_image()
 			self.display.blit(texture, texture.get_rect(center=robot.position))
 
-	def update(self):
-		if self.inputs.keys["primary"].keydown:
-			self.switch_robot_control()
-
-		for robot in self.robots:
-			robot.update(self.inputs, self.robots)
-			if self.tilemap.collide_with_danger(robot):
-				if robot.controlled: self.switch_robot_control()
-				self.robots.remove(robot)
-
-		self.level_timer += 1
-		if self.level_timer == self.level_period/4:
-			self.tilemap.set_warnings(self.current_level)
-		elif self.level_timer == self.level_period*3/4:
-			self.tilemap.set_dangers(self.current_level)
-		elif self.level_timer == self.level_period:
-			self.tilemap.clear_level()
-		elif self.level_timer > self.level_period:
-			self.level_timer = 0
-			self.level_period = max(LEVEL_PERIOD_MIN*60, self.level_period - (LEVEL_PERIOD_MALUS*60))
-
-			# difficulty needs to be factored in
-			self.current_level = self.data.easy_levels[randint(0, len(self.data.easy_levels)-1)]
-
-
-	def switch_robot_control(self):
-		self.controlled_robot.controlled = False
-		self.controlled_robot = self.next_robot
-		self.controlled_robot.controlled = True
-		self.controlled_robot.is_next = False
-		self.next_robot = self.robots[randint(0, len(self.robots)-1)]
-		while (self.next_robot == self.controlled_robot) and (len(self.robots) > 1):
-			self.next_robot = self.robots[randint(0, len(self.robots)-1)]
-		self.next_robot.is_next = True
-
 	def exit_condition(self):
-		return len(self.robots) < 1
+		return len(self.robots) < 3
 
 
 if __name__ == "__main__":
